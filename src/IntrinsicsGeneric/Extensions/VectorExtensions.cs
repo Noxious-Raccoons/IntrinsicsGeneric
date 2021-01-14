cb2d6e80ba4b5b376e1934503ed310009fcebbb6
Dilorfin <pavel.kuybida@gmail.com>
2021-01-14 16:38:34 +0200

    Add vector extensions

---
+++ src/IntrinsicsGeneric/Extensions/VectorExtensions.cs
@@ -0,0 +1,34 @@
+﻿using IntrinsicsGeneric.InternalHelpers;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+
+namespace IntrinsicsGeneric.Extensions
+{
+    public static class VectorExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T Sum<T>(this Vector128<T> vector)
+            where T : unmanaged
+        {
+            var sum = default(T);
+            for (var i = 0; i < Vector128<T>.Count; i++)
+            {
+                sum = MathUnsafe<T>.Add(sum, vector.GetElement(i));
+            }
+
+            return sum;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T Sum<T>(Vector256<T> a)
+            where T : unmanaged
+        {
+            var sum = default(T);
+            for (var i = 0; i < Vector256<T>.Count; i++)
+            {
+                sum = MathUnsafe<T>.Add(sum, a.GetElement(i));
+            }
+            return sum;
+        }
+    }
+}
---
+++ src/IntrinsicsGeneric/Extensions/VectorHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+
+namespace IntrinsicsGeneric.Extensions
+{
+    public class VectorHelper<T>
+        where T : unmanaged
+    {
+        public static Vector256<T> AllBitsSet128
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+#if NET5_0
+                return Vector256<T>.AllBitsSet;
+#elif NETCOREAPP3_1
+                return Vector256.Create(0xFFFFFFFF).As<uint, T>();
+#endif
+            }
+        }
+
+        public static Vector128<T> AllBitsSet256
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+#if NET5_0
+                return Vector128<T>.AllBitsSet;
+#elif NETCOREAPP3_1
+                return Vector128.Create(0xFFFFFFFF).As<uint, T>();
+#endif
+            }
+        }
+    }
+}
