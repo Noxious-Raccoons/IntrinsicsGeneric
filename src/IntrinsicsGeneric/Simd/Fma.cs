3ba199875214806f162c6a71321a957fbf4860a4
Dilorfin <pavel.kuybida@gmail.com>
2021-01-14 16:57:24 +0200

    Add empty Fma

---
+++ src/IntrinsicsGeneric/Simd/Fma.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace IntrinsicsGeneric.Simd
+{
+    public abstract class Fma<T> : Avx<T>
+        where T : unmanaged
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static Vector128<T> MultiplyAdd(Vector128<T> a, Vector128<T> b, Vector128<T> c)
+        {
+            if (typeof(T) == typeof(float))
+            {
+                return Fma.MultiplyAdd(a.As<T, float>(), b.As<T, float>(), c.As<T, float>()).As<float, T>();
+            }
+            if (typeof(T) == typeof(double))
+            {
+                return Fma.MultiplyAdd(a.As<T, double>(), b.As<T, double>(), c.As<T, double>()).As<double, T>();
+            }
+
+            Sse2.X64.ConvertScalarToVector128Double(a.As<T, double>(), 15);
+            throw new NotSupportedException();
+        }
+    }
+}
