cd21cf4816087aa29f6dcdc8713804aef52fb405
Dilorfin <pavel.kuybida@gmail.com>
2021-01-14 23:29:19 +0200

    Fix AllBitsSet

--- src/IntrinsicsGeneric/Extensions/VectorHelper.cs
+++ src/IntrinsicsGeneric/Extensions/VectorHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 
 namespace IntrinsicsGeneric.Extensions
@@ -8,7 +9,20 @@ namespace IntrinsicsGeneric.Extensions
     {
         protected VectorHelper() { }
 
-        public static Vector256<T> AllBitsSet128
+        public static Vector128<T> AllBitsSet128
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+#if NETCOREAPP3_1
+                return Vector128.Create(0xFFFFFFFF).As<uint, T>();
+#else
+                return Vector128<T>.AllBitsSet;
+#endif
+            }
+        }
+
+        public static Vector256<T> AllBitsSet256
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
@@ -21,16 +35,37 @@ namespace IntrinsicsGeneric.Extensions
             }
         }
 
-        public static Vector128<T> AllBitsSet256
+        public static Vector128<T> RandomBitsSet128
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
             {
-#if NETCOREAPP3_1
-                return Vector128.Create(0xFFFFFFFF).As<uint, T>();
-#else
-                return Vector128<T>.AllBitsSet;
-#endif
+                var random = new Random();
+                var a1 = random.Next();
+                var a2 = random.Next();
+                var a3 = random.Next();
+                var a4 = random.Next();
+
+                return Vector128.Create(a1, a2, a3, a4).As<int, T>();
+            }
+        }
+
+        public static Vector256<T> RandomBitsSet256
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                var random = new Random();
+                var a1 = random.Next();
+                var a2 = random.Next();
+                var a3 = random.Next();
+                var a4 = random.Next();
+                var a5 = random.Next();
+                var a6 = random.Next();
+                var a7 = random.Next();
+                var a8 = random.Next();
+
+                return Vector256.Create(a1, a2, a3, a4, a5, a6, a7, a8).As<int, T>();
             }
         }
     }
