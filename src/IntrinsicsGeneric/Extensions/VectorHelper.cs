f8b794d3cb0c6b6db94575b675b015a36f7d7e91
Dilorfin <pavel.kuybida@gmail.com>
2021-01-15 11:06:14 +0200

    Fix AllBitsSet (#7)
    * Add RandomBitsSet

--- src/IntrinsicsGeneric/Extensions/VectorHelper.cs
+++ src/IntrinsicsGeneric/Extensions/VectorHelper.cs
@@ -1,14 +1,29 @@
-﻿using System.Runtime.CompilerServices;
+﻿using IntrinsicsGeneric.Simd;
+using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 
 namespace IntrinsicsGeneric.Extensions
 {
-    public class VectorHelper<T>
+    public unsafe class VectorHelper<T>
         where T : unmanaged
     {
         protected VectorHelper() { }
 
-        public static Vector256<T> AllBitsSet128
+        public static Vector128<T> AllBitsSet128
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+#if NETCOREAPP3_1
+                return Vector128.Create(0xFFFFFFFF).As<uint, T>();
+#else
+                return Vector128<T>.AllBitsSet;
+#endif
+            }
+        }
+
+        public static Vector256<T> AllBitsSet256
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
@@ -21,16 +36,35 @@ namespace IntrinsicsGeneric.Extensions
             }
         }
 
-        public static Vector128<T> AllBitsSet256
+        public static Vector128<T> RandomBitsSet128
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
             {
-#if NETCOREAPP3_1
-                return Vector128.Create(0xFFFFFFFF).As<uint, T>();
-#else
-                return Vector128<T>.AllBitsSet;
-#endif
+                var random = new Random();
+                byte[] bytes = new byte[16];
+                random.NextBytes(bytes);
+
+                fixed (byte* ptr = bytes)
+                {
+                    return Sse2<byte>.LoadVector128(ptr).As<byte, T>();
+                }
+            }
+        }
+
+        public static Vector256<T> RandomBitsSet256
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                var random = new Random();
+                byte[] bytes = new byte[32];
+                random.NextBytes(bytes);
+
+                fixed (byte* ptr = bytes)
+                {
+                    return Avx2<byte>.LoadVector256(ptr).As<byte, T>();
+                }
             }
         }
     }
