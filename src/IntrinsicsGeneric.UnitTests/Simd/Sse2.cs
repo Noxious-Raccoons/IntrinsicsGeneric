335b96e49be2429bcf3d0eae6372208008be9acc
Dilorfin <pavel.kuybida@gmail.com>
2021-01-15 16:18:36 +0200

    Add unit tests for Sse2

--- src/IntrinsicsGeneric.UnitTests/Simd/Sse2.cs
+++ src/IntrinsicsGeneric.UnitTests/Simd/Sse2.cs
@@ -7,6 +7,8 @@ namespace IntrinsicsGeneric.UnitTests.Simd
     [TestFixture]
     public class Sse2
     {
+        #region Add
+
         [TestCase(1, 2)]
         [TestCase(1U, 2U)]
         [TestCase(1F, 2F)]
@@ -49,6 +51,10 @@ namespace IntrinsicsGeneric.UnitTests.Simd
 
             Assert.AreEqual(expectedVector, result);
         }
+
+        #endregion
+
+        #region And
 
         [TestCase((sbyte)1, (sbyte)0, (sbyte)0)]
         [TestCase((sbyte)1, (sbyte)1, (sbyte)1)]
@@ -120,13 +126,13 @@ namespace IntrinsicsGeneric.UnitTests.Simd
 
             Assert.AreEqual(expectedVector, result);
         }
-        
+
         [Test]
         public void AndFloat_Additional()
         {
             var vectorA = Vector128.Create(5F);
             var vectorB = Vector128.Create(10F);
-            
+
             var expectedVector = System.Runtime.Intrinsics.X86.Sse.And(vectorA, vectorB);
             var result = Sse2<float>.And(vectorA, vectorB);
 
@@ -138,11 +144,137 @@ namespace IntrinsicsGeneric.UnitTests.Simd
         {
             var vectorA = Vector128.Create(5D);
             var vectorB = Vector128.Create(10D);
-            
+
             var expectedVector = System.Runtime.Intrinsics.X86.Sse2.And(vectorA, vectorB);
             var result = Sse2<double>.And(vectorA, vectorB);
 
             Assert.AreEqual(expectedVector, result);
         }
+
+        #endregion
+
+        #region LoadVector128
+
+        [Test]
+        public void LoadVector128()
+        {
+            LoadVector128(new byte[] {15, 75, 42, 37, 15, 75, 42, 37, 15, 75, 42, 37, 15, 75, 42, 37});
+            LoadVector128(new sbyte[] {15, -75, 42, 37, 15, -75, 42, 37, 15, -75, 42, 37, 15, -75, 42, 37});
+
+            LoadVector128(new ushort[] {15, 75, 42, 37, 15, 75, 42, 37});
+            LoadVector128(new short[] {15, -75, 42, 37, 15, -75, 42, 37});
+
+            LoadVector128(new uint[]{15, 75, 42, 37});
+            LoadVector128(new int[] {15, -75, 42, 37});
+
+            LoadVector128(new ulong[]{42, 37});
+            LoadVector128(new long[]{42, -37});
+
+            LoadVector128(new float[]{15, 75, 42, 37});
+            LoadVector128(new double[]{42, 37});
+        }
+
+        private unsafe void LoadVector128<T>(T[] arr)
+            where T : unmanaged
+        {
+            Assert.AreEqual(Vector128<T>.Count, arr.Length);
+            fixed (T* ptr = arr)
+            {
+                var vector = Sse2<T>.LoadVector128(ptr);
+                for (var i = 0; i < Vector128<T>.Count; i++)
+                {
+                    Assert.AreEqual(vector.GetElement(i), arr[i]);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Store
+
+        [Test]
+        public void Store()
+        {
+            Store(new byte[] {15, 75, 42, 37, 15, 75, 42, 37, 15, 75, 42, 37, 15, 75, 42, 37});
+            Store(new sbyte[] {15, -75, 42, 37, 15, -75, 42, 37, 15, -75, 42, 37, 15, -75, 42, 37});
+
+            Store(new ushort[] {15, 75, 42, 37, 15, 75, 42, 37});
+            Store(new short[] {15, -75, 42, 37, 15, -75, 42, 37});
+
+            Store(new uint[]{15, 75, 42, 37});
+            Store(new int[] {15, -75, 42, 37});
+
+            Store(new ulong[]{42, 37});
+            Store(new long[]{42, -37});
+
+            Store(new float[]{15, 75, 42, 37});
+            Store(new double[]{42, 37});
+        }
+
+        private unsafe void Store<T>(T[] arr)
+            where T : unmanaged
+        {
+            Assert.AreEqual(Vector128<T>.Count, arr.Length);
+
+            var vector = Simd<T>.CreateVector128(default);
+            fixed (T* ptr = arr)
+            {
+                Sse2<T>.Store(ptr, vector);
+            }
+
+            for (var i = 0; i < Vector128<T>.Count; i++)
+            {
+                Assert.AreEqual(default(T), arr[i]);
+            }
+        }
+
+        #endregion
+        
+        #region Subtract
+
+        [TestCase(1, 0)]
+        [TestCase(1U, 0U)]
+        [TestCase(1F, 0F)]
+        [TestCase(1L, 0L)]
+        [TestCase(1UL, 0UL)]
+        [TestCase(1D, 0D)]
+        [TestCase((short)1, (short)0)]
+        [TestCase((ushort)1, (ushort)0)]
+        [TestCase((byte)1, (byte)0)]
+        [TestCase((sbyte)1, (sbyte)0)]
+        public void Subtract<T>(T value, T expected)
+            where T : unmanaged
+        {
+            var vector = Simd<T>.CreateVector128(value);
+            var expectedVector = Simd<T>.CreateVector128(expected);
+
+            var result = Sse2<T>.Subtract(vector, vector);
+
+            Assert.AreEqual(expectedVector, result);
+        }
+
+        [TestCase(42, 2, 40)]
+        [TestCase(3U, 2U, 1U)]
+        [TestCase(3F, 2F, 1F)]
+        [TestCase(3L, 2L, 1L)]
+        [TestCase(3UL, 2UL, 1UL)]
+        [TestCase(5D, 2D, 3D)]
+        [TestCase((short)5, (short)2, (short)3)]
+        [TestCase((ushort)5, (ushort)2, (ushort)3)]
+        [TestCase((byte)5, (byte)2, (byte)3)]
+        [TestCase((sbyte)5, (sbyte)2, (sbyte)3)]
+        public void Subtract<T>(T a, T b, T expected)
+            where T : unmanaged
+        {
+            var vectorA = Simd<T>.CreateVector128(a);
+            var vectorB = Simd<T>.CreateVector128(b);
+            var expectedVector = Simd<T>.CreateVector128(expected);
+
+            var result = Sse2<T>.Subtract(vectorA, vectorB);
+
+            Assert.AreEqual(expectedVector, result);
+        }
+
+        #endregion
     }
 }
