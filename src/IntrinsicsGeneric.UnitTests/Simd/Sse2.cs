649403235200e5c97f2f9616f5071c8a7df5224c
Dilorfin <pavel.kuybida@gmail.com>
2021-01-15 12:47:40 +0200

    Update And tests

--- src/IntrinsicsGeneric.UnitTests/Simd/Sse2.cs
+++ src/IntrinsicsGeneric.UnitTests/Simd/Sse2.cs
@@ -50,73 +50,98 @@ namespace IntrinsicsGeneric.UnitTests.Simd
             Assert.AreEqual(expectedVector, result);
         }
 
-        [TestCase(1, 0)]
-        [TestCase(1, 1)]
-        [TestCase(0b1010, 0b1010)]
-        [TestCase(0b1010, 0b0101)]
-        [TestCase(0b10101010, 0b10101010)]
-        [TestCase(0b10101010, 0b00111010)]
-        public void AndInt(int a, int b)
+        [TestCase((sbyte)1, (sbyte)0, (sbyte)0)]
+        [TestCase((sbyte)1, (sbyte)1, (sbyte)1)]
+        [TestCase((sbyte)0b1010, (sbyte)0b1010, (sbyte)0b1010)]
+        [TestCase((sbyte)0b1010, (sbyte)0b0101, (sbyte)0)]
+        [TestCase((sbyte)0b00101010, (sbyte)0b00101010, (sbyte)0b00101010)]
+        [TestCase((sbyte)0b00101010, (sbyte)0b00111000, (sbyte)0b00101000)]
+        [TestCase((byte)1, (byte)0, (byte)0)]
+        [TestCase((byte)1, (byte)1, (byte)1)]
+        [TestCase((byte)0b1010, (byte)0b1010, (byte)0b1010)]
+        [TestCase((byte)0b1010, (byte)0b0101, (byte)0)]
+        [TestCase((byte)0b10101010, (byte)0b10101010, (byte)0b10101010)]
+        [TestCase((byte)0b10101010, (byte)0b00111010, (byte)0b00101010)]
+        [TestCase((short)1, (short)0, (short)0)]
+        [TestCase((short)1, (short)1, (short)1)]
+        [TestCase((short)0b1010, (short)0b1010, (short)0b1010)]
+        [TestCase((short)0b1010, (short)0b0101, (short)0)]
+        [TestCase((short)0b10101010, (short)0b10101010, (short)0b10101010)]
+        [TestCase((short)0b10101010, (short)0b00111010, (short)0b00101010)]
+        [TestCase((ushort)1, (ushort)0, (ushort)0)]
+        [TestCase((ushort)1, (ushort)1, (ushort)1)]
+        [TestCase((ushort)0b1010, (ushort)0b1010, (ushort)0b1010)]
+        [TestCase((ushort)0b1010, (ushort)0b0101, (ushort)0)]
+        [TestCase((ushort)0b10101010, (ushort)0b10101010, (ushort)0b10101010)]
+        [TestCase((ushort)0b10101010, (ushort)0b00111010, (ushort)0b00101010)]
+        [TestCase(1, 0, 0)]
+        [TestCase(1, 1, 1)]
+        [TestCase(0b1010, 0b1010, 0b1010)]
+        [TestCase(0b1010, 0b0101, 0)]
+        [TestCase(0b10101010, 0b10101010, 0b10101010)]
+        [TestCase(0b10101010, 0b00111010, 0b00101010)]
+        [TestCase((uint)1, (uint)0, (uint)0)]
+        [TestCase((uint)1, (uint)1, (uint)1)]
+        [TestCase((uint)0b1010, (uint)0b1010, (uint)0b1010)]
+        [TestCase((uint)0b1010, (uint)0b0101, (uint)0)]
+        [TestCase((uint)0b10101010, (uint)0b10101010, (uint)0b10101010)]
+        [TestCase((uint)0b10101010, (uint)0b00111010, (uint)0b00101010)]
+        [TestCase((float)1, (float)0, (float)0)]
+        [TestCase((float)1, (float)1, (float)1)]
+        [TestCase((float)0b1010, (float)0b1010, (float)0b1010)]
+        [TestCase((float)0b10101010, (float)0b10101010, (float)0b10101010)]
+        [TestCase((float)0b10101010, (float)0b00111010, (float)0b00101010)]
+        [TestCase((double)1, (double)0, (double)0)]
+        [TestCase((double)1, (double)1, (double)1)]
+        [TestCase((double)0b1010, (double)0b1010, (double)0b1010)]
+        [TestCase((double)0b10101010, (double)0b10101010, (double)0b10101010)]
+        [TestCase((double)0b10101010, (double)0b00111010, (double)0b00101010)]
+        [TestCase((long)1, (long)0, (long)0)]
+        [TestCase((long)1, (long)1, (long)1)]
+        [TestCase((long)0b1010, (long)0b1010, (long)0b1010)]
+        [TestCase((long)0b1010, (long)0b0101, (long)0)]
+        [TestCase((long)0b10101010, (long)0b10101010, (long)0b10101010)]
+        [TestCase((long)0b10101010, (long)0b00111010, (long)0b00101010)]
+        [TestCase((ulong)1, (ulong)0, (ulong)0)]
+        [TestCase((ulong)1, (ulong)1, (ulong)1)]
+        [TestCase((ulong)0b1010, (ulong)0b1010, (ulong)0b1010)]
+        [TestCase((ulong)0b1010, (ulong)0b0101, (ulong)0)]
+        [TestCase((ulong)0b10101010, (ulong)0b10101010, (ulong)0b10101010)]
+        [TestCase((ulong)0b10101010, (ulong)0b00111010, (ulong)0b00101010)]
+        public void And<T>(T a, T b, T expected)
+            where T : unmanaged
         {
-            var vectorA = Vector128.Create(a);
-            var vectorB = Vector128.Create(b);
+            var vectorA = Simd<T>.CreateVector128(a);
+            var vectorB = Simd<T>.CreateVector128(b);
 
-            var expectedVector = System.Runtime.Intrinsics.X86.Sse2.And(vectorA, vectorB);
+            var expectedVector = Simd<T>.CreateVector128(expected);
 
-            var result = Sse2<int>.And(vectorA, vectorB);
+            var result = Sse2<T>.And(vectorA, vectorB);
+
+            Assert.AreEqual(expectedVector, result);
+        }
+        
+        [Test]
+        public void AndFloat_Additional()
+        {
+            var vectorA = Vector128.Create(5F);
+            var vectorB = Vector128.Create(10F);
             
+            var expectedVector = System.Runtime.Intrinsics.X86.Sse.And(vectorA, vectorB);
+            var result = Sse2<float>.And(vectorA, vectorB);
+
             Assert.AreEqual(expectedVector, result);
         }
 
-        [TestCase(1, 0)]
-        [TestCase(1, 1)]
-        [TestCase(0b1010, 0b1010)]
-        [TestCase(0b1010, 0b0101)]
-        [TestCase(0b10101010, 0b10101010)]
-        [TestCase(0b10101010, 0b00111010)]
-        public void AndLong(long a, long b)
+        [Test]
+        public void AndDouble_Additional()
         {
-            var vectorA = Vector128.Create(a);
-            var vectorB = Vector128.Create(b);
-
-            var expectedVector = System.Runtime.Intrinsics.X86.Sse2.And(vectorA, vectorB);
-
-            var result = Sse2<long>.And(vectorA, vectorB);
+            var vectorA = Vector128.Create(5D);
+            var vectorB = Vector128.Create(10D);
             
-            Assert.AreEqual(expectedVector, result);
-        }
-
-        [TestCase(1, 0)]
-        [TestCase(1, 1)]
-        [TestCase(0b1010, 0b1010)]
-        [TestCase(0b1010, 0b0101)]
-        [TestCase(0b10101010, 0b10101010)]
-        [TestCase(0b10101010, 0b00111010)]
-        public void AndShort(short a, short b)
-        {
-            var vectorA = Vector128.Create(a);
-            var vectorB = Vector128.Create(b);
-
             var expectedVector = System.Runtime.Intrinsics.X86.Sse2.And(vectorA, vectorB);
+            var result = Sse2<double>.And(vectorA, vectorB);
 
-            var result = Sse2<short>.And(vectorA, vectorB);
-            
-            Assert.AreEqual(expectedVector, result);
-        }
-
-        [TestCase(1, 0)]
-        [TestCase(1, 1)]
-        [TestCase(0b1010, 0b1010)]
-        [TestCase(0b1010, 0b0101)]
-        public void AndByte(byte a, byte b)
-        {
-            var vectorA = Vector128.Create(a);
-            var vectorB = Vector128.Create(b);
-
-            var expectedVector = System.Runtime.Intrinsics.X86.Sse2.And(vectorA, vectorB);
-
-            var result = Sse2<byte>.And(vectorA, vectorB);
-            
             Assert.AreEqual(expectedVector, result);
         }
     }
