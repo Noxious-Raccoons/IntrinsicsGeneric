0f475b8ae9a61f38712b489d8236bee1717ec44e
Dilorfin <pavel.kuybida@gmail.com>
2021-01-17 14:15:53 +0200

    Add ToBitArray

--- src/IntrinsicsGeneric/Extensions/VectorExtensions.cs
+++ src/IntrinsicsGeneric/Extensions/VectorExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using IntrinsicsGeneric.InternalHelpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 
@@ -55,6 +58,84 @@ namespace IntrinsicsGeneric.Extensions
             }
 
             return result;
+        }
+
+        public static unsafe BitArray ToBitArray<T>(this Vector128<T> vector)
+            where T : unmanaged
+        {
+            List<byte> bytes = new List<byte>(sizeof(T) * Vector128<T>.Count);
+
+            for (int i = 0; i < Vector128<T>.Count; i++)
+            {
+                bytes.AddRange(GetBytes(vector.GetElement(i)));
+            }
+
+            return new BitArray(bytes.ToArray());
+        }
+
+        public static unsafe BitArray ToBitArray<T>(this Vector256<T> vector)
+            where T : unmanaged
+        {
+            List<byte> bytes = new List<byte>(sizeof(T) * Vector256<T>.Count);
+
+            for (int i = 0; i < Vector256<T>.Count; i++)
+            {
+                bytes.AddRange(GetBytes(vector.GetElement(i)));
+            }
+
+            return new BitArray(bytes.ToArray());
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static byte[] GetBytes<T>(T value)
+            where T : unmanaged
+        {
+            if (typeof(T) == typeof(float))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, float>(ref value));
+            }
+            if (typeof(T) == typeof(double))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, double>(ref value));
+            }
+
+            if (typeof(T) == typeof(int))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, int>(ref value));
+            }
+            if (typeof(T) == typeof(uint))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, uint>(ref value));
+            }
+
+            if (typeof(T) == typeof(long))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, long>(ref value));
+            }
+            if (typeof(T) == typeof(ulong))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, ulong>(ref value));
+            }
+
+            if (typeof(T) == typeof(short))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, short>(ref value));
+            }
+            if (typeof(T) == typeof(ushort))
+            {
+                return BitConverter.GetBytes(Unsafe.As<T, ushort>(ref value));
+            }
+
+            if (typeof(T) == typeof(sbyte))
+            {
+                return new[] { (byte)Unsafe.As<T, sbyte>(ref value) };
+            }
+            if (typeof(T) == typeof(byte))
+            {
+                return new[] { Unsafe.As<T, byte>(ref value) };
+            }
+
+            throw new NotSupportedException();
         }
     }
 }
